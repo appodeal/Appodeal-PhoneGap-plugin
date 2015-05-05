06a2447fedac149df3bcb8c32db4a860dbe1868b
Nazar <nzrkzk@gmail.com>
2015-05-06 01:39:15 +0300

    Updating to Appodeal 1.11.2

--- src/android/AppodealPlugin.java
+++ src/android/AppodealPlugin.java
@@ -5,41 +5,238 @@ import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import android.os.Handler;
+import android.os.Looper;
+
 import com.appodeal.ads.Appodeal;
+import com.appodeal.ads.InterstitialCallbacks;
+import com.appodeal.ads.VideoCallbacks;
 
+public class AppodealPlugin extends CordovaPlugin {
 
-public class AppodealPlugin extends CordovaPlugin{
+	private static final String ACTION_INIT_APPODEAL = "initialize";
+	private static final String ACTION_INIT_APPODEAL_AD_TYPE = "initializeAdType";
+	private static final String ACTION_SET_INTERSTITIAL_CALLBACKS = "enableIntertitialCallbacks";
+	private static final String ACTION_SET_VIDEO_CALLBACKS = "enableVideoCallbacks";
+	private static final String ACTION_SHOW = "show";
+	private static final String ACTION_SHOW_WITH_PRICE_FLOOR = "showWithPriceFloor";
+	private static final String ACTION_SET_AUTO_CACHE = "setAutoCache";
+	private static final String ACTION_CACHE_BANNER = "cacheBanner";
+	private static final String ACTION_SET_ON_LOADED_TRIGGER_BOTH = "setOnLoadedTriggerBoth";
+	private static final String ACTION_DISABLE_NETWORK = "disableNetwork";
 	
-	
-	private static final String ACTION_INI_APPODEAL = "initialize";
-	private static final String ACTION_SHOW_BANNER = "showBanner";
-	private static final String ACTION_IS_LOADED = "isLoaded";
-	
+	private String appKey = null;
+	private int adType = 7;
+	private boolean autoCache = true;
+	private boolean setOnTriggerBoth = true;
+	private boolean setInterstitialCallbacks = false;
+	private boolean setVideoCallbacks = false;
+
 	@Override
-	public boolean execute(String action, JSONArray args, CallbackContext callback) throws JSONException{
-		
-		if (action.equals(ACTION_INI_APPODEAL)) {
-			final String appKey = args.getString(0);
-			
+	public boolean execute(String action, JSONArray args,
+			CallbackContext callback) throws JSONException {
+
+		if (action.equals(ACTION_INIT_APPODEAL)) {
+			appKey = args.getString(0);
 			cordova.getActivity().runOnUiThread(new Runnable() {
 				@Override
 				public void run() {
 					Appodeal.initialize(cordova.getActivity(), appKey);
 				}
 			});
-			
 			return true;
-		} else if (action.equals(ACTION_SHOW_BANNER)){
+		} else if (action.equals(ACTION_INIT_APPODEAL_AD_TYPE)) {
+			appKey = args.getString(0);
+			adType = args.getInt(1);
 			cordova.getActivity().runOnUiThread(new Runnable() {
 				@Override
 				public void run() {
-					Appodeal.showBanner(cordova.getActivity());
+					Appodeal.initialize(cordova.getActivity(), appKey, adType);
 				}
 			});
-		} else if (action.equals(ACTION_IS_LOADED)){
-			Appodeal.isLoaded();
+		} else if (action.equals(ACTION_SET_INTERSTITIAL_CALLBACKS)) {
+			setInterstitialCallbacks = args.getBoolean(0);
+			cordova.getActivity().runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					if(setInterstitialCallbacks) {
+						Appodeal.setInterstitialCallbacks(interstitialListener);
+					}
+				}
+			});
+		} else if (action.equals(ACTION_SET_VIDEO_CALLBACKS)) {
+			setVideoCallbacks = args.getBoolean(0);
+			cordova.getActivity().runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					if(setVideoCallbacks) {
+						Appodeal.setVideoCallbacks(videoListener);
+					}
+				}
+			});
+		} else if (action.equals(ACTION_SHOW)) {
+			adType = args.getInt(0);
+			cordova.getActivity().runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					Appodeal.show(cordova.getActivity(), adType);
+				}
+			});
+		} else if (action.equals(ACTION_SHOW_WITH_PRICE_FLOOR)) {
+			adType = args.getInt(0);
+			cordova.getActivity().runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					Appodeal.showWithPriceFloor(cordova.getActivity(), adType);
+				}
+			});
+		} else if (action.equals(ACTION_SET_AUTO_CACHE)) {
+			adType = args.getInt(0);
+			autoCache = args.getBoolean(1);
+			cordova.getActivity().runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					Appodeal.setAutoCache(adType, autoCache);
+				}
+			});
+		} else if (action.equals(ACTION_CACHE_BANNER)) {
+			adType = args.getInt(0);
+			cordova.getActivity().runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					Appodeal.cache(cordova.getActivity(), adType);
+				}
+			});
+		} else if (action.equals(ACTION_SET_ON_LOADED_TRIGGER_BOTH)) {
+			adType = args.getInt(0);
+			setOnTriggerBoth = args.getBoolean(1);
+			cordova.getActivity().runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					Appodeal.setOnLoadedTriggerBoth(adType, setOnTriggerBoth);
+				}
+			});
+		} else if (action.equals(ACTION_DISABLE_NETWORK)) {
+			final String network = args.getString(0);
+			cordova.getActivity().runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					Appodeal.disableNetwork(network);
+				}
+			});
 		}
-		
 		return false;
 	}
+	
+	private InterstitialCallbacks interstitialListener = new InterstitialCallbacks() {
+		
+		@Override
+		public void onInterstitialShown() {
+			new Handler(Looper.getMainLooper()).post(new Runnable() {
+			    @Override
+			    public void run() {
+			    	webView.loadUrl ("javascript:cordova.fireDocumentEvent('onInterstitialShown');");
+			    }
+			});
+		}
+		
+		@Override
+		public void onInterstitialLoaded(boolean arg0) {
+			new Handler(Looper.getMainLooper()).post(new Runnable() {
+			    @Override
+			    public void run() {
+			    	webView.loadUrl ("javascript:cordova.fireDocumentEvent('onInterstitialLoaded');");
+			    }
+			});
+		}
+		
+		@Override
+		public void onInterstitialFailedToLoad() {
+			new Handler(Looper.getMainLooper()).post(new Runnable() {
+			    @Override
+			    public void run() {
+			    	webView.loadUrl ("javascript:cordova.fireDocumentEvent('onInterstitialFailedToLoad');");
+			    }
+			});
+		}
+		
+		@Override
+		public void onInterstitialClosed() {
+			new Handler(Looper.getMainLooper()).post(new Runnable() {
+			    @Override
+			    public void run() {
+			    	webView.loadUrl ("javascript:cordova.fireDocumentEvent('onInterstitialClosed');");
+			    }
+			});
+		}
+		
+		@Override
+		public void onInterstitialClicked() {
+			new Handler(Looper.getMainLooper()).post(new Runnable() {
+			    @Override
+			    public void run() {
+			    	webView.loadUrl ("javascript:cordova.fireDocumentEvent('onInterstitialClicked');");
+			    }
+			});
+		}
+	};
+	
+	private VideoCallbacks videoListener = new VideoCallbacks() {
+
+		@Override
+		public void onVideoClosed() {
+			new Handler(Looper.getMainLooper()).post(new Runnable() {
+			    @Override
+			    public void run() {
+			    	webView.loadUrl ("javascript:cordova.fireDocumentEvent('onVideoClosed');");
+			    }
+			});
+		}
+
+		@Override
+		public void onVideoFailedToLoad() {
+			new Handler(Looper.getMainLooper()).post(new Runnable() {
+			    @Override
+			    public void run() {
+			    	webView.loadUrl ("javascript:cordova.fireDocumentEvent('onVideoFailedToLoad');");
+			    }
+			});
+		}
+
+		@Override
+		public void onVideoFinished() {
+			new Handler(Looper.getMainLooper()).post(new Runnable() {
+			    @Override
+			    public void run() {
+			    	webView.loadUrl ("javascript:cordova.fireDocumentEvent('onVideoFinished');");
+			    }
+			});
+		}
+
+		@Override
+		public void onVideoLoaded() {
+			new Handler(Looper.getMainLooper()).post(new Runnable() {
+			    @Override
+			    public void run() {
+					new Handler(Looper.getMainLooper()).post(new Runnable() {
+					    @Override
+					    public void run() {
+					    	webView.loadUrl ("javascript:cordova.fireDocumentEvent('onVideoLoaded');");
+					    }
+					});
+			    }
+			});
+		}
+
+		@Override
+		public void onVideoShown() {
+			new Handler(Looper.getMainLooper()).post(new Runnable() {
+			    @Override
+			    public void run() {
+			    	webView.loadUrl ("javascript:cordova.fireDocumentEvent('onVideoShown');");
+			    }
+			});
+		}
+		
+	};
 }
--- www/appodeal.js
+++ www/appodeal.js
@@ -1,13 +1,55 @@
-
+cordova.define("com.appodeal.plugin.AppodealPlugin", function(require, exports, module) { 
 	var Appodeal = exports;
 
 	var exec = require('cordova/exec');
 	var cordova = require('cordova');
+	
+	adType = {
+	  NONE: '0',
+	  INTERSTITIAL: '1',
+	  VIDEO: '2',
+	  ALL: '7',
+	  ANY: '7'
+	};
 
 	Appodeal.initialize = function(appKey) {
 		exec(null, null, "AppodealPlugin", "initialize", [appKey]);
 	}
-
-	Appodeal.showBanner = function() {
-		exec(null, null, "AppodealPlugin", "showBanner", []);
+	
+	Appodeal.initialize = function(appKey, adType) {
+		exec(null, null, "AppodealPlugin", "initializeAdType", [appKey, adType]);
 	}
+	
+	Appodeal.enableInterstitialCallbacks = function(listener) {
+		exec(null, null, "AppodealPlugin", "enableIntertitialCallbacks", [listener]);
+	}
+	
+	Appodeal.enableVideoCallbacks = function(listener) {
+		exec(null, null, "AppodealPlugin", "enableVideoCallbacks", [listener]);
+	}
+
+	Appodeal.show = function(adType) {
+		exec(null, null, "AppodealPlugin", "show", [adType]);
+	}
+	
+	Appodeal.showWithPriceFloor = function(adType) {
+		exec(null, null, "AppodealPlugin", "showWithPriceFloor", [adType]);
+	}
+	
+	Appodeal.setAutoCache = function(adType, autoCache) {
+		exec(null, null, "AppodealPlugin", "setAutoCache", [adType, autoCache]);
+	}
+	
+	Appodeal.cache = function(adType) {
+		exec(null, null, "AppodealPlugin", "cacheBanner", [adType]);
+	}
+	
+	Appodeal.setOnLoadedTriggerBoth = function(adType, setTrigger) {
+		exec(null, null, "AppodealPlugin", "setOnLoadedTriggerBoth", [adType, setTrigger]);
+	}
+	
+	Appodeal.disableNetwork = function(network) {
+		exec(null, null, "AppodealPlugin", "disableNetwork", [network]);
+	}
+
+});
