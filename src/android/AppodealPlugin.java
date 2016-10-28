c5387cabcb760016ec3ccac8b33e8c5568355069
Nazar Kozak <nzrkzk@gmail.com>
2016-10-28 13:43:15 +0300

    Update AppodealPlugin.java

--- src/android/AppodealPlugin.java
+++ src/android/AppodealPlugin.java
@@ -539,13 +539,13 @@ public class AppodealPlugin extends CordovaPlugin {
         if((adtype & 2) > 0) {
             type |= Appodeal.SKIPPABLE_VIDEO;
         }
-        if(((adtype & 4) > 0) {
+        if((adtype & 4) > 0) {
             type |= Appodeal.BANNER;
         }
-        if(((adtype & 8) > 0) {
+        if((adtype & 8) > 0) {
             type |= Appodeal.BANNER_BOTTOM;
         }
-        if(((adtype & 16) > 0) {
+        if((adtype & 16) > 0) {
             type |= Appodeal.BANNER_TOP;
         }
         if((adtype & 128) > 0) {
